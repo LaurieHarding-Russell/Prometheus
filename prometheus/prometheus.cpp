0122c46ec93051bc21678db063f1692095f0bda7
Prometheus <lauriehardingrussell@gmail.com>
2016-05-19 20:43:17 +0000

    test commit

--- prometheus/prometheus.cpp
+++ prometheus/prometheus.cpp
@@ -8,15 +8,23 @@
 #include "tvControl.h"
 #include "computer.h"
 #include "internet.h"
-#include <thread>
-#include <chrono>
+
 #include <mutex>
 #include <queue>
+#include <thread>
+#include <chrono>
 
 static void getMicInput();
 std::queue<std::string> input;
 
-namespace m{
+/*
+namespace m is an ugly hack to get around the fact that lirc has a public variable quit as well.
+I might make a class that handles microphone input, but past attempts have lead to
+"error: invalid use of member. * in static member function"
+Maybe pocketsphinx forces the class to be static? Or I'm missing a stupid mistake
+*/
+
+namespace m {
 	bool quit = false;
 	std::mutex lock;
 }
@@ -35,23 +43,27 @@ int main() {
 			command = input.front();
 			input.pop();
 			if (command == "computer") {
+				std::cout << "Computer: ";
 				int num = 0;
 				while (input.empty() && num <20) {
-					std::this_thread::sleep_for(std::chrono::milliseconds(100));
+					std::this_thread::sleep_for(std::chrono::milliseconds(10));
 					num++;
 				}
 				if (input.empty())
 					continue;
 
-	                        command = input.front();
-       		                input.pop();
+				command = input.front();
+				input.pop();
 				if (command == "search") {
 					command = "";
 					while(!input.empty()) {
-		        	                command += input.front();
-	        	        	        input.pop();
+						command += input.front();
+						input.pop();
 					}
-					internet->search(command.c_str());
+					std::string result = internet->search(command.c_str());
+					computer.say(result.c_str());
+					std::cout<< result << '\n';
+
 				} else if (command == "tv"){
 					if (input.empty())
 						continue;
@@ -59,16 +71,17 @@ int main() {
        		        	        input.pop();
 				} else if (command == "say") {
 					std::string say = "";
-					while(!input.empty() && command != "computer") {
+					 std::this_thread::sleep_for(std::chrono::milliseconds(100));
+					while(!input.empty()) {
 						say = say + input.front();
 						input.pop();
 					}
+					std::this_thread::sleep_for(std::chrono::milliseconds(100));
 					computer.say(say.c_str());
+					std::cout << "The computer said" << say << '\n';
 				} else {
 					std::cout << command + "\n";
 				}
-			} else {
-				std::cout << command + "\n";
 			}
 		}
 
@@ -92,7 +105,7 @@ static void getMicInput() {
         config = cmd_ln_init(NULL, ps_args(), TRUE,
                 "-hmm", MODELDIR "/en-us/en-us",
                 "-lm", MODELDIR "/en-us/en-us.lm.bin",
-                "-dict", MODELDIR "/en-us/cmudict-en-us.dict",
+                "-dict", MODELDIR "/en-us/k-en-us.dict",
                 NULL);
 
         ps = ps_init(config);
@@ -102,7 +115,7 @@ static void getMicInput() {
 	uint8 utt_started, in_speech;
 	int32 k;
 	char const *hyp;
-	bool failed;
+	bool failed = false;
 
  	if ((ad = ad_open_dev(cmd_ln_str_r(config, "-adcdev"),
             			(int) cmd_ln_float32_r(config,
@@ -112,20 +125,21 @@ static void getMicInput() {
 		failed = true;
 
 	if (ps_start_utt(ps) < 0)
-		printf("utter problems");
-
+		failed =true;
 	std::string command = "";
 	m::lock.lock();
-	while (!quit) {
+	while (!m::quit && !failed) {
 		m::lock.unlock();
 		k = ad_read(ad, adbuf, 4096);
+		if ( k < 0)
+			std::cout << "crap \n";
 		ps_process_raw(ps, adbuf, k, false, false);
 		in_speech = ps_get_in_speech(ps);
 		if (in_speech && !utt_started) {
           		utt_started = TRUE;
        		}
         	if (!in_speech && utt_started) {
-            		// speech -> silence transition, time to start new utterance 
+            		// speech -> silence transition, time to start new utterance
             		ps_end_utt(ps);
             		hyp = ps_get_hyp(ps, NULL );
             		if (hyp != NULL) {
@@ -146,7 +160,6 @@ static void getMicInput() {
 		m::lock.lock();
 	}
 	m::lock.unlock();
-	std::cout << failed;
 	ad_close(ad);
         ps_free(ps);
         cmd_ln_free_r(config);
