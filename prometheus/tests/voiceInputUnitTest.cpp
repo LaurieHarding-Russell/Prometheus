1b1e00706fb1911023cc929f5598a297bc544009
Prometheus <lauriehardingrussell@gmail.com>
2016-05-30 20:06:39 +0000

    Commenting out voice library test. It works fine, but segment faults in the test.

--- prometheus/tests/voiceInputUnitTest.cpp
+++ prometheus/tests/voiceInputUnitTest.cpp
@@ -1,16 +1,31 @@
 /*
 By Laurie Harding-Russell
 Purpose: Unit testing voiceInput.
+
+NOTE: DO NOT USE. Test segment faults for some unknown reason. The library works in prometheus...
+I don't know why.
 */
 
 #include "voiceInput.h"
 #include <thread>
 #include <iostream>
 
+void mainMenuInputTest();
+
 int main() {
-	std::thread inputThread = std::thread(&startGeneralInput);
+	mainMenuInputTest();
+}
+
+void mainMenuInputTest() {
+	std::thread inputThread = std::thread(&mainMenuInput);
 	for (int lp1 =0; lp1 != 1000; lp1++) {
+		std::string command;
+		do {
+			command = getInput();
+			std::cout << command << "\n";
+		} while (command !="");
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
+	stopInput();
 	inputThread.join();
 }
